c32e19ee4fe27907a9051fb074da60a5f3532358
jeondui <jeondui@gmail.com>
2023-08-16 19:32:12 +0900

    프로젝트 초기 구성

---
+++ MC_backend/src/main/java/com/memory/connect/McBackendApplication.java
@@ -0,0 +1,13 @@
+package com.memory.connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McBackendApplication.class, args);
+    }
+
+}
