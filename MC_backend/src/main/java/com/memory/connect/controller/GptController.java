f188bee900000f5bc337c4f11f469d70b5b67c41
dltjdgh0428 <dltjdgh0428@naver.com>
2023-08-18 17:11:10 +0900

    오류 수정 : CORS 문제해결

--- MC_backend/src/main/java/com/memory/connect/controller/GptController.java
+++ MC_backend/src/main/java/com/memory/connect/controller/GptController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @Slf4j
 @RequestMapping("/api/v1/chat-gpt")
+@CrossOrigin(origins = "*")
 public class GptController {
     private final SpeechToTextService speechToTextService;
     private final DataService questionService;
