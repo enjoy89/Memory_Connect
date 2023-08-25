f8a160d76849f76975d3cd5d0f69fbad4ac7ae2c
usablechan <lch8910@naver.com>
2023-08-25 16:03:35 +0900

    Dd

---
+++ MC_frontend/MemoryConnectFlutter/m_c/lib/screens/loading_scree.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
