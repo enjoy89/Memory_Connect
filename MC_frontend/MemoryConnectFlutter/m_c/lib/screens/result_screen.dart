c34f6943714a9609393abbba8fce14b44e2d9300
usablechan <lch8910@naver.com>
2023-08-27 01:48:49 +0900

    dd

--- MC_frontend/MemoryConnectFlutter/m_c/lib/screens/result_screen.dart
+++ MC_frontend/MemoryConnectFlutter/m_c/lib/screens/result_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class ResultScreen extends StatefulWidget {
   const ResultScreen({super.key});
@@ -15,22 +16,59 @@ class _ResultScreenState extends State<ResultScreen> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            const SizedBox(height: 20),
-            const Text(
-              '메모리 커넥트 결과',
-              style: TextStyle(
-                fontSize: 35,
-                fontWeight: FontWeight.bold,
-              ),
+            Row(
+              children: [
+                Image.network(
+                    "https://careerup-client.s3.ap-northeast-2.amazonaws.com/logo.png",
+                    width: 100,
+                    height: 100),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  "결과 : ",
+                  style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                FutureBuilder(
+                  future: getResult(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      // 데이터를 성공적으로 받아왔을 때 표시할 위젯
+                      return Text(
+                        snapshot.data.toString(),
+                        style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      );
+                    } else {
+                      // 데이터를 아직 받아오지 못했을 때 표시할 위젯
+                      return Text('데이터를 아직 받아오지 못했습니다.');
+                    }
+                  },
+                ),
+                Text(
+                  "/8 ",
+                  style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+              ],
             ),
-            const SizedBox(height: 10),
-            Center(
+            Container(
+              decoration: BoxDecoration(color: Colors.white),
+              width: 300,
+              height: 300,
               child: Image.network(
-                'https://careerup-client.s3.ap-northeast-2.amazonaws.com/result.png',
-                height: MediaQuery.of(context).size.height / 2.5,
+                "https://careerup-client.s3.ap-northeast-2.amazonaws.com/result.png",
+                width: 300,
+                height: 300,
               ),
             ),
-            const SizedBox(height: 10),
             Container(
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.height / 1.5,
@@ -99,5 +137,19 @@ class _ResultScreenState extends State<ResultScreen> {
         ),
       ),
     );
+  }
+}
+
+Future<String> getResult() async {
+  final response =
+      await http.get(Uri.parse('http://localhost:8088/api/result'));
+  if (response.statusCode == 200) {
+    print("아래는 정답의 갯수");
+    print(response.body);
+    print("결과를 받아왔습니다!");
+    return response.body;
+  } else {
+    print("getResult에서 에러가 발생했습니다");
+    throw Exception('Failed to load data');
   }
 }
